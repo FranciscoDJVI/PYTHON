9ee08800f51f8e0fb072311cc0b72465736a92e4
Francisco Vanegas <vanegasfrancisco415@gmail.com>
2025-03-15 02:03:12 -0500

    Test in django project practice

--- Django/djangotutorial/mysite/polls/views.py
+++ Django/djangotutorial/mysite/polls/views.py
@@ -1,29 +1,44 @@
+from datetime import timezone
+import re
 from django.db.models import F
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect 
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
-from . models import Choice, Question
+from .models import Choice, Question
 # Create your views here.
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
-    
+
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
-    
+        """
+        Return the last five published qeustions (not including
+        those set to be
+        published in the future).
+        """
+        Question.objects.filter(pub_date__lte=timezone.now().order_by("-pub_date")[:5])
+
+
 class DetailView(generic.DateDetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
 class ResultView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-    
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -33,17 +48,15 @@ def vote(request, question_id):
             request,
             "polls/detail.html",
             {
-                "question":question,
-                "error_message":"You didn't select a choice.",
+                "question": question,
+                "error_message": "You didn't select a choice.",
             },
         )
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
-        
-        
+
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-    
 
 
 """def index(request):
@@ -89,3 +102,4 @@ def vote(request, question_id):
         
         
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))"""
+
